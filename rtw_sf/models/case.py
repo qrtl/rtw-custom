99c2a15c1927be8b7db163b88d57cc203862d2a3
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-04-20 10:14:07 +0900

    フィールド追加

--- rtw_sf/models/case.py
+++ rtw_sf/models/case.py
@@ -10,3 +10,5 @@ class rtw_sf_case(models.Model):
 
     subject = fields.Char('Subject')
     contacts = fields.Many2one('res.partner', "Contact", copy=False)  # コンタクト
+    accounts = fields.Many2one('res.partner', "Account", copy=False)  # アカウント
+    parents = fields.Many2one('rtw_sf_case', "Parents", copy=False)  # 親ケース
