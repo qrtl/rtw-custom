e0c4c95d108ac8924f32388be572f4659dbe4124
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-05-18 09:47:04 +0900

    問い合わせ更新

--- rtw_inquiry/__manifest__.py
+++ rtw_inquiry/__manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/inquiry.xml',
+        # 'views/res_partner.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
---
+++ rtw_inquiry/models/res_partner.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api
+
+
+class rtw_sf_partner_oppo(models.Model):
+    _inherit = "res.partner"
+    _description = 'inquiry'
+
+    inquiry = fields.One2many('rtw_sf.inquiry', inverse_name='contact_person')
+    inquiry_count = fields.Integer(string="inquiry count", compute="_compute_inquiry_count")
+
+    def _compute_inquiry_count(self):
+        for rec in self:
+            inquiry_count = self.env['rtw_sf.inquiry'].search_count([('contact_person', '=', rec.id)])
+            rec.inquiry_count = inquiry_count
+
+    def action_open_inquiry(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'c',
+            'res_model': 'rtw_sf.inquiry',
+            'domain': [('contact_person', '=', self.id)],
+            'view_mode': 'tree,form',
+            'target': 'current',
+            'context': {
+                'default_id': self.id,
+                'default_contact_person': self.id,
+            }
+        }
