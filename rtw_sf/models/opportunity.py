0798baeb3609b77190cb7f72d68dfbb34533b9eb
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-05-19 09:10:22 +0900

    OppoturnityをCRMへ統合中

--- rtw_sf/models/opportunity.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class rtw_sf_opportunity(models.Model):
-    _name = 'opportunity'
-    _description = 'case'
-    _rec_name = "subject"
