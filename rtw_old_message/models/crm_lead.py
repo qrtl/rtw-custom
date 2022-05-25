14864e8aaf3f6e04decb23a65cacf53744061593
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-05-25 16:25:33 +0900

    チャターログ修正

--- rtw_old_message/models/crm_lead.py
+++ rtw_old_message/models/crm_lead.py
@@ -14,7 +14,10 @@ class rtw_old_message_crm_lead(models.Model):
     # @api.depends("id")
     def _compute_message_ids(self):
         for record in self:
-            full_id = record.get_metadata()[0].get("xmlid")
-            xml_id = full_id.split('.')[1]
-            print(xml_id)
-            record.old_messages = self.env['rtw.old_message'].search([('parent_id', '=', xml_id)])
+            record.old_messages = [(5,0,0)]
+            if record.get_metadata()[0].get("xmlid"):
+                full_id = record.get_metadata()[0].get("xmlid")
+                print(full_id)
+                xml_id = full_id.split('.')[1]
+                print(xml_id)
+                record.old_messages = self.env['rtw.old_message'].search([('parent_id', '=', xml_id)])
