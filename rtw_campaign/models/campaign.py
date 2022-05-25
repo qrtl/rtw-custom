00889a3cd751ca62ec57968c3ed13c0d3ee64121
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-05-25 15:55:47 +0900

    キャンペーン修正・連絡先検索修正

--- rtw_campaign/models/campaign.py
+++ rtw_campaign/models/campaign.py
@@ -5,23 +5,34 @@ from odoo import models, fields, api
 
 class rtw_old_message_crm_lead(models.Model):
     _inherit = 'utm.campaign'
+    currency_id = fields.Many2one(related="company_id.currency_id", readonly=True)
 
     parent_id = fields.Many2one('utm.campaign', string="utm.campaign")
     start_date = fields.Datetime('StartDate')
+
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  required=True, readonly=True,
+                                  states={'draft': [('readonly', False)]},
+                                  default=lambda
+                                      self: self.env.company.currency_id.id)
     budgeted_cost = fields.Monetary(
         string="BudgetedCost",
         default=0.0,
+        currency_field="currency_id",
     )
     actual_cost = fields.Monetary(
         string="ActualCost",
         default=0.0,
+        currency_field="currency_id",
     )
     description = fields.Text("Description")
     amount_all_opportunities = fields.Monetary(
         string="AmountAllOpportunities",
         default=0.0,
+        currency_field="currency_id",
     )
     amount_won_opportunities = fields.Monetary(
         string="AmountWonOpportunities",
+        currency_field="currency_id",
         default=0.0,
     )
