563e14ee97df5e3e253ff5329ab9215898604359
bando0319 <y_bando@leverage-system.jp>
2022-07-22 18:25:53 +0900

    連絡先変更履歴表示（翻訳修正有）

--- rtw_sf/models/res_partner.py
+++ rtw_sf/models/res_partner.py
@@ -9,12 +9,12 @@ class rtw_sf_partner(models.Model):
     # A ID
     is_deleted = fields.Boolean("IsDeleted", default=0)  # B 削除フラグ
 
-    fax = fields.Char(string="fax")  # X OK
-    contact_type = fields.Many2one('res.partner.contact_type', string="type")  # E OK
-    channel = fields.Many2one('res.partner.channel', 'channel')  # BG OK
-    account_number = fields.Char("AccountNumber")  # Y OK
-    annual_revenue = fields.Char("AnnualRevenue")  # AC 年間売上 OK
-    number_of_employees = fields.Integer("NumberOfEmployees")  # AD 従業員数 OK
+    fax = fields.Char(string="fax", tracking=True)  # X OK
+    contact_type = fields.Many2one('res.partner.contact_type', string="type", tracking=True)  # E OK
+    channel = fields.Many2one('res.partner.channel', 'channel', tracking=True)  # BG OK
+    account_number = fields.Char("AccountNumber", tracking=True)  # Y OK
+    annual_revenue = fields.Char("AnnualRevenue", tracking=True)  # AC 年間売上 OK
+    number_of_employees = fields.Integer("NumberOfEmployees", tracking=True)  # AD 従業員数 OK
     ownership = fields.Selection([
         ('1', '公開'),
         ('2', '非公開'),
@@ -22,7 +22,7 @@ class rtw_sf_partner(models.Model):
         ('4', 'その他'),
     ], default='',
         string="ownership", )  # OK
-    ticker_symbol = fields.Char("TickerSymbol")  # AF 銘柄コード OK
+    ticker_symbol = fields.Char("TickerSymbol", tracking=True)  # AF 銘柄コード OK
     description = fields.Text("Description")  # OK
     rating = fields.Selection([
         ('1', '重要顧客'),
@@ -34,22 +34,22 @@ class rtw_sf_partner(models.Model):
         ('6', '見込み無'),
     ], default='',
         string="Rating")  # ok
-    site = fields.Char("Site")  # OK
-    supplier_no = fields.Char("supplier_no")  # OK 仕入先番号 Field5__c
-    kana = fields.Char("kana")  # OK フリガナ Field5__c
-    corporate_mail = fields.Boolean("corporate_mail", default=0)  # 法人宛郵便 OK  Field6__c
-    transactions = fields.Many2one('res.partner.transactions', string="transactions")  # 取引方法 OK Field4__c
-    payment_terms_1 = fields.Char("payment_terms_1")  # 支払い条件１ OK X1__c
-    payment_terms_2 = fields.Char("payment_terms_2")  # 支払い条件2 OK X2__c
-    multiplier_black = fields.Float("multiplier_black")  # 掛率(黒) OK Field7__c
-    multiplier_green = fields.Float("multiplier_green")  # 掛率(黒) OK Field8__c
+    site = fields.Char("Site", tracking=True)  # OK
+    supplier_no = fields.Char("supplier_no", tracking=True)  # OK 仕入先番号 Field5__c
+    kana = fields.Char("kana", tracking=True)  # OK フリガナ Field5__c
+    corporate_mail = fields.Boolean("corporate_mail", default=0, tracking=True)  # 法人宛郵便 OK  Field6__c
+    transactions = fields.Many2one('res.partner.transactions', string="transactions", tracking=True)  # 取引方法 OK Field4__c
+    payment_terms_1 = fields.Char("payment_terms_1", tracking=True)  # 支払い条件１ OK X1__c
+    payment_terms_2 = fields.Char("payment_terms_2", tracking=True)  # 支払い条件2 OK X2__c
+    multiplier_black = fields.Float("multiplier_black", tracking=True)  # 掛率(黒) OK Field7__c
+    multiplier_green = fields.Float("multiplier_green", tracking=True)  # 掛率(黒) OK Field8__c
     fare_payment_terms = fields.Selection([
         ('1', '運賃込み(設置まで)'),
         ('2', '運賃別途'),
         ('3', '運賃込み(デポ入れまで)'),
         ('4', '運賃込み（設置まで）'),
     ], default='',
-        string="fare_payment_terms")  # 支払条件運賃 OK Field9__c
+        string="fare_payment_terms", tracking=True)  # 支払条件運賃 OK Field9__c
     categorization = fields.Selection([
         ('1', '大手HM'),
         ('2', 'その他HM'),
@@ -100,47 +100,47 @@ class rtw_sf_partner(models.Model):
         ('4', '未取引'),
         ('5', '営業対象外'),
     ], default='1',
-        string="situation")  # 状況 OK Field11__c
+        string="situation", tracking=True)  # 状況 OK Field11__c
     address_confirmation_required = fields.Selection([('moving', '（不達）転居'), ('unknown', '（不達）不明')],
-                                                     string="address_confirmation_required")  # 住所要確認 OK Field12__c
+                                                     string="address_confirmation_required", tracking=True)  # 住所要確認 OK Field12__c
     last_contract_date = fields.Datetime("last_contract_date")  # 最新成約日 OK Field13__c
     total_number_of_transactions = fields.Integer("total_number_of_transactions")  # 累計取引回数 OK Field14__c
     cumulative_sales = fields.Float("cumulative_sales")  # 累計売上金額 OK Field15__c
     potential = fields.Selection([('a', 'A'), ('b', 'B'), ('c', 'C'), ('d', 'D')], string="potential",
                                  default='')  # ポテンシャル OK Field16__c
     kubun = fields.Selection([('internal', '国内'), ('external', '海外')], string="kubun")  # 区分 OK  Field20__c
-    rival_companies = fields.Boolean("rival_companies", default=0)  # 競合他社 OK Field23__c
+    rival_companies = fields.Boolean("rival_companies", default=0, tracking=True)  # 競合他社 OK Field23__c
     group = fields.Char("group")  # G OK Field21__c
     transaction_category = fields.Selection([
         ('sundries', '諸口'),
         ('continuation', '継続'),
     ], default='sundries',
-        string="transaction_category")  # 取引区分 OK Field27__c
-    campaign_1 = fields.Char("campaign_1")  # キャンペーン① OK Field25__c
-    campaign_2 = fields.Char("campaign_2")  # キャンペーン② OK Field26__c
-    dummy = fields.Boolean("dummy", default=0)  # ダミー OK Field29__c
+        string="transaction_category", tracking=True)  # 取引区分 OK Field27__c
+    campaign_1 = fields.Char("campaign_1", tracking=True)  # キャンペーン① OK Field25__c
+    campaign_2 = fields.Char("campaign_2", tracking=True)  # キャンペーン② OK Field26__c
+    dummy = fields.Boolean("dummy", default=0, tracking=True)  # ダミー OK Field29__c
     # relation
 
     case = fields.One2many('rtw_sf_case', inverse_name='contacts')  # ケース OK
     # contact
-    account_id = fields.Many2one('res.partner', "AccountId")  # OK
+    account_id = fields.Many2one('res.partner', "AccountId", tracking=True)  # OK
     first_name = fields.Char("FirstName")  # OK
     last_name = fields.Char("LastName")  # OK
     record_type = fields.Char("RecordTypeId")  # OK
-    other_country = fields.Char("OtherCountry")  # OK
-    other_postal_code = fields.Char("OtherPostalCode")  # OK
-    other_state = fields.Char("OtherState")  # OK
-    other_city = fields.Char("OtherCity")  # OK
-    other_street = fields.Char("OtherStreet")  # OK
-    mailing_country = fields.Char("MailingCountry")  # OK
-    mailing_postal_code = fields.Char("MailingPostalCode")  # OK
-    mailing_state = fields.Char("MailingState")  # OK
-    mailing_city = fields.Char("MailingCity")  # OK
-    mailing_street = fields.Char("MailingStreet")  # OK
-    home_phone = fields.Char("HomePhone")  # OK
-    other_phone = fields.Char("OtherPhone")  # OK
+    other_country = fields.Char("OtherCountry", tracking=True)  # OK
+    other_postal_code = fields.Char("OtherPostalCode", tracking=True)  # OK
+    other_state = fields.Char("OtherState", tracking=True)  # OK
+    other_city = fields.Char("OtherCity", tracking=True)  # OK
+    other_street = fields.Char("OtherStreet", tracking=True)  # OK
+    mailing_country = fields.Char("MailingCountry", tracking=True)  # OK
+    mailing_postal_code = fields.Char("MailingPostalCode", tracking=True)  # OK
+    mailing_state = fields.Char("MailingState", tracking=True)  # OK
+    mailing_city = fields.Char("MailingCity", tracking=True)  # OK
+    mailing_street = fields.Char("MailingStreet", tracking=True)  # OK
+    home_phone = fields.Char("HomePhone", tracking=True)  # OK
+    other_phone = fields.Char("OtherPhone", tracking=True)  # OK
     reports_to_id = fields.Char("ReportsToId")  # OK
-    department = fields.Char("Department")  # OK
+    department = fields.Char("Department", tracking=True)  # OK
     lead_source = send_catalog = fields.Selection([
         ('1', '通常営業'),
         ('2', 'リサーチ・新規開拓'),
@@ -200,21 +200,21 @@ class rtw_sf_partner(models.Model):
         ('55', '納品時の案内用紙'),
         ('56', '雑誌'),
     ], default='',
-        string="LeadSource")  # OK
-    birthdate = fields.Datetime(string="Birthdate")  # OK
-    opt_out_email = fields.Boolean(string="HasOptedOutOfEmail", default=0)  # OK
+        string="LeadSource", tracking=True)  # OK
+    birthdate = fields.Datetime(string="Birthdate", tracking=True)  # OK
+    opt_out_email = fields.Boolean(string="HasOptedOutOfEmail", default=0, tracking=True)  # OK
     opt_out_fax = fields.Boolean(string="HasOptedOutOfFax", default=0)  # ok
     do_not_call = fields.Boolean(string="DoNotCall", default=0)  # OK
     last_cu_request_date = fields.Datetime("LastCURequestDate")  # OK
     last_cu_update_date = fields.Datetime("LastCUUpdateDate")  # OK
     email_bounced_reason = fields.Text("EmailBouncedReason")  # OK
     email_bounced_date = fields.Datetime("EmailBouncedDate")  # OK
-    ny_card = fields.Boolean("ny_card", default=0)  # 年賀状 OK Field1__c
-    need_dm = fields.Boolean("need_dm", default=0)  # DM希望 OK DM__c
+    ny_card = fields.Boolean("ny_card", default=0, tracking=True)  # 年賀状 OK Field1__c
+    need_dm = fields.Boolean("need_dm", default=0, tracking=True)  # DM希望 OK DM__c
     gift = fields.Boolean("gift", default=0,
                           help="Customers, business partners, and suppliers who need to send gifts, gifts, mid-year gifts, New Year's gifts, and year-end gifts"
                           )  # 進物 OK Field2__c2
-    summer_greeting = fields.Boolean("summer_greeting", default=0)  # 暑中見舞 OK Field3__c
+    summer_greeting = fields.Boolean("summer_greeting", default=0, tracking=True)  # 暑中見舞 OK Field3__c
     send_catalog = fields.Selection([
         ('send', '送付'),
         ('holdings ', '手持'),
@@ -230,17 +230,17 @@ class rtw_sf_partner(models.Model):
         ('7', '女'),
 
     ], default='',
-        string="gender")  # 性別 OK Field6__c
-    in_has = fields.Char("in_has")  # R社内所有者 OK R__c
-    do_not_dm = fields.Boolean("do_not_dm", default=0)  # DM不要 OK DoNotDM
+        string="gender", tracking=True)  # 性別 OK Field6__c
+    in_has = fields.Char("in_has", tracking=True)  # R社内所有者 OK R__c
+    do_not_dm = fields.Boolean("do_not_dm", default=0, tracking=True)  # DM不要 OK DoNotDM
     evaluation = fields.Selection([
         ('A', 'A優良顧客'),
         ('B', 'B平均的'),
         ('C', 'C見込み有'),
         ('D', 'D見込み無'),
     ], default='',
-        string="evaluation", help="2015年～2017年　売上金額ベースの評価")  # 評価 OK Field8__c
-    dm_unknown = fields.Boolean("dm_unknown", help=">DM欄　未記入", default=0)  # DM不明 OK DM_unknown__c
+        string="evaluation", help="2015年～2017年　売上金額ベースの評価", tracking=True)  # 評価 OK Field8__c
+    dm_unknown = fields.Boolean("dm_unknown", help=">DM欄　未記入", default=0, tracking=True)  # DM不明 OK DM_unknown__c
     action_1 = fields.Char("action1")  # アクション1⃣ OK
     action_2 = fields.Char("action2")  # アクション② OK
     related_attributes = fields.Selection([
@@ -258,15 +258,15 @@ class rtw_sf_partner(models.Model):
         ('-', '以下不使用'),
         ('12', '施主（その他住宅）'),
     ], default='',
-        string="related_attributes")  # 関係者属性 OK HM_OB__c
-    cs_number = fields.Char("cs_number")  # 取引先番号 OK Field12__c
+        string="related_attributes", tracking=True)  # 関係者属性 OK HM_OB__c
+    cs_number = fields.Char("cs_number", tracking=True)  # 取引先番号 OK Field12__c
     mc_subscriber = fields.Many2one('res.partner', "Contact", copy=False)  # コンタクト OK
-    haihai_id = fields.Char("ID__c")  # OK
-    mail_change = fields.Boolean("mail_change", default=0)  # メール変更 OK Field14_mail__c
-    haihai_open_count = fields.Integer("haihai_open_count")  # 配配開封数 OKField14__c
-    calender = fields.Boolean("calender", default=0)  # ｶﾚﾝﾀﾞｰ OKcalendar__c
+    haihai_id = fields.Char("ID__c", tracking=True)  # OK
+    mail_change = fields.Boolean("mail_change", default=0, tracking=True)  # メール変更 OK Field14_mail__c
+    haihai_open_count = fields.Integer("haihai_open_count", tracking=True)  # 配配開封数 OKField14__c
+    calender = fields.Boolean("calender", default=0, tracking=True)  # ｶﾚﾝﾀﾞｰ OKcalendar__c
     key_man = fields.Boolean("key_man", default=0)  # キーマン OKField17__c
-    r_owner = fields.Boolean("r_owner", default=0, help="Ritzwell家具の購入実績がある")  # Rオーナー OKR_1__c
+    r_owner = fields.Boolean("r_owner", default=0, help="Ritzwell家具の購入実績がある", tracking=True)  # Rオーナー OKR_1__c
     sansancard = fields.Boolean("sansancard",
                                 default=0)  # Created by Scan to Salesforce OKsansancard__CreatedByScanToSalesforce__c
     retirement = fields.Char("retirement")  # 退職 OKField18__c
@@ -285,8 +285,8 @@ class rtw_sf_partner(models.Model):
         ('9', '家具店・IC等からの紹介'),
         ('10', 'その他'),
     ], default='',
-        string="lead_source_roc")  # リードソース(ROC) OKROC__c
-    interest_roc = fields.Char("interest_roc")  # 興味・関心(ROC) OK ROC_2__c
+        string="lead_source_roc", tracking=True)  # リードソース(ROC) OKROC__c
+    interest_roc = fields.Char("interest_roc", tracking=True)  # 興味・関心(ROC) OK ROC_2__c
     Influencers = fields.Selection([
         ('h', '高'),
         ('m', '中'),
@@ -321,10 +321,10 @@ class rtw_sf_partner(models.Model):
         ('25', '植木職、造園師'),
         ('26', 'その他'),
     ], default='',
-        string="occupational_category")  # 職種 OK Field23__c
-    not_use = fields.Boolean("not_use", default=0)  # 使用不可 OK DM_4__c
-    mail2 = fields.Char("mail2")  # メール2 OK Field24__c
-    mail_1_2 = fields.Boolean("mail_1_2", default=0)  # メール1・2 OK X1_2__c
+        string="occupational_category", tracking=True)  # 職種 OK Field23__c
+    not_use = fields.Boolean("not_use", default=0, tracking=True)  # 使用不可 OK DM_4__c
+    mail2 = fields.Char("mail2", tracking=True)  # メール2 OK Field24__c
+    mail_1_2 = fields.Boolean("mail_1_2", default=0, tracking=True)  # メール1・2 OK X1_2__c
     haihai_count = fields.Integer("haihai_count")  # 配配送信数 OK Field25__c2
     region = fields.Selection([
         ('1', '東北・北海道'),
@@ -337,11 +337,11 @@ class rtw_sf_partner(models.Model):
         ('8', '中国'),
         ('9', '四国'),
         ('10', '九州・沖縄'),
-    ], default='',)
+    ], default='', tracking=True)
     # region = fields.Char(compute="_get_region", string="region", store=True, ondelete='CASCADE')  # 地域 OK Field34__c
-    stop_letter = fields.Boolean("stop_letter", default=0)  # LETTER停止 OK LETTER__c
+    stop_letter = fields.Boolean("stop_letter", default=0, tracking=True)  # LETTER停止 OK LETTER__c
     # kin = fields.Char("Field36__c")  # 親類（親戚・家族） OK
-    kin = fields.Many2one('res.partner', "kin", copy=False)  # Field36__c
+    kin = fields.Many2one('res.partner', "kin", copy=False, tracking=True)  # Field36__c
     mail3 = fields.Char("mail3")  # メール3 OK X3__c
     company_name = fields.Char("company_name")  # 会社名 OK Field37__c
     x20 = fields.Boolean("X20__c", default=0)  # OK
@@ -351,7 +351,7 @@ class rtw_sf_partner(models.Model):
     x60 = fields.Boolean("X60__c", default=0)  # OK
     x70 = fields.Boolean("X70__c", default=0)  # OK
     x80 = fields.Boolean("X80__c", default=0)  # OK
-    joint_name = fields.Char("X1__c")  # 連名1 OK
+    joint_name = fields.Char("X1__c", tracking=True)  # 連名1 OK
     web = fields.Selection([
         ('fb', 'FB'),
         ('insta', 'insta'),
@@ -361,7 +361,7 @@ class rtw_sf_partner(models.Model):
         ('youtube', 'Youtube'),
         ('other', 'その他'),
     ], string="web",
-        default='')  # WEBサイト OK WEB__c
+        default='', tracking=True)  # WEBサイト OK WEB__c
     media_name = fields.Selection([
         ('1', 'モダンリビング'),
         ('2', "I'm home."),
@@ -376,12 +376,12 @@ class rtw_sf_partner(models.Model):
         ('11', 'その他'),
 
     ], default='',
-        string="media_name")  # 媒体名 OK Field39__c
+        string="media_name", tracking=True)  # 媒体名 OK Field39__c
     case_count = fields.Integer(string="case count", compute="_compute_case_count")
     no_hyphen_phone = fields.Char("no_hyphen_phone", compute="_get_phone_non_hyphen", store=True)
     no_hyphen_mobile = fields.Char("no_hyphen_mobile", compute="_get_phone_non_mobile", store=True)
 
-    age = fields.Integer("age", compute="_get_age")
+    age = fields.Integer("age", compute="_get_age", tracking=True)
 
     condition = fields.Many2many('res.partner.condition', track_visibility='always')
 
