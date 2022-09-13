27238912604a68e03485f06a95bdde88ac86ca07
Kazushi Eguchi <sandwhale1010@gmail.com>
2022-09-13 11:39:11 +0900

    修正

--- rtw_excel_report/__manifest__.py
+++ rtw_excel_report/__manifest__.py
@@ -20,13 +20,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'excel_import_export', 'sale_management'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'report/proforma.xml',
+        'report/report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
